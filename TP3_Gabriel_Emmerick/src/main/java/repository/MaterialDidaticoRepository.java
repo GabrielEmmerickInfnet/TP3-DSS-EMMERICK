0bc7c2cf9f73faf2dad20ae6a9654538d51f9646
Prey <Prey@192.168.0.117>
2024-06-18 22:26:53 -0300

    tp3infnet

---
+++ TP3_Gabriel_Emmerick/src/main/java/repository/MaterialDidaticoRepository.java
@@ -0,0 +1,5 @@
+package repository;
+
+public interface MaterialDidaticoRepository {
+
+}
---
+++ TP3_Gabriel_Emmerick/src/test/java/com/example/TP3_Gabriel_Emmerick/Tp3GabrielEmmerickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TP3_Gabriel_Emmerick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3GabrielEmmerickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
